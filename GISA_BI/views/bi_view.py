6e6d0b2be53900060789d5d5880f369e277963d6
RSNF <ray1001novaes@gmail.com>
2024-12-07 01:05:59 -0300

    update view

--- GISA_BI/views/bi_view.py
+++ GISA_BI/views/bi_view.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse, HttpResponse
-from GISA_BI.services import OpenAIService
+from GISA_BI.services import OpenAIService, DataFrameService
 from GISA_BI.serializers import GisaRequestSerializer
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
@@ -7,13 +7,24 @@ from rest_framework.decorators import action
 
 class GisaBIView(viewsets.GenericViewSet):
     openai = OpenAIService("data/dados-covid-df.csv")
+    df_service = DataFrameService("data/dados-covid-df.csv")
 
-    @action(methods=["post"], detail=True, url_path="search-embed")
-    def search(self, request):
+    @action(methods=["get"], detail=False, url_path="embed")
+    def embed(self, request):
 
-        df = self.openai.search_embed()
+        print(self.openai.df.head()["Faixa Etária"])
 
-        return JsonResponse(df, status=status.HTTP_200_OK)
+        list = self.openai.df.head()["Faixa Etária"].to_list()
+
+        response = self.openai.client.embeddings.create(
+            input=list,
+            model="text-embedding-3-small",
+        )
+
+        embeddings = [item.embedding for item in response.data]
+
+        return HttpResponse(embeddings, status=status.HTTP_200_OK)
+        # return JsonResponse(embeddings, status=status.HTTP_200_OK)
 
     @action(methods=["post"], detail=False, url_path="insight")
     def insight(self, request):
@@ -25,3 +36,10 @@ class GisaBIView(viewsets.GenericViewSet):
             status=status.HTTP_200_OK,
             safe=False,
         )
+
+    @action(methods=["get"], detail=False, url_path="obitos_ra")
+    def get_df_obitos_ra(self, request):
+
+        df_obitos_ra = self.df_service.get_obitos_ra()
+
+        return JsonResponse(df_obitos_ra.to_dict(), status=status.HTTP_200_OK)
